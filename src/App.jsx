c556ad50af6b31c6aecd6332c72436f7fdf79bb7
het-dwt <156792047+het-dwt@users.noreply.github.com>
2024-02-05 11:49:32 +0530

    Update via branch1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,129 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//(1)....Bydefault_templete-code
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// (2)....using_fetch-api_in_react.
+// import { useEffect } from "react";
+// import { useState } from "react";
+
+// function App() {
+//   const [apiData, setApiState] = useState();
+//   useEffect(() => {
+//     fetch("https://dummyjson.com/products")
+//       .then((response) => response.json())
+//       .then((json) => console.log(json));
+//   }, []);
+
+//   return (
+//     <div>
+//       Different ways to fetch Data
+//       {setApiState}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//(3)....Using_Axios-Package.
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [apiData, setApiData] = useState([]);
+  useEffect(() => {
+    axios.get("https://dummyjson.com/products").then((response) => {
+      console.log(response.data);
+      setApiData(response.data);
+    });
+  }, []);
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Sr:</th>
+              <th>Product</th>
+              <th>Product Name:</th>
+              <th>Rating:</th>
+              <th>Price:</th>
+            </tr>
+          </thead>
+          <tbody>
+            {apiData.products?.map((o) => (
+              <tr key={o.id}>
+                <td>{o.id}</td>
+                <td><img src={o.thumbnail} width="80"/></td>
+                <td>{o.title}</td>
+                <td>{o.rating}/5</td>
+                <td>{o.price}/-</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+// <div>{JSON.stringify(apiData.products)}</div>
+// <div>Id: {apiData.products?.map((o) => o.id)}</div>
+// <div>Title: {apiData.products?.map((o) => o.title)}</div>
+
+//(4)Async & await.....
+// import { useEffect } from "react";
+// import { useState } fsrom "react";
+// import axios from "axios";
+
+// function App() {
+//   const [apiData, setApiState] = {};
+//   useEffect(() => {
+//     //async
+//     (async () => {
+//       try {
+//         //await
+//         const response = await axios.get(
+//           "https://dummyjson.com/products/category/smartphones"
+//         );
+//         console.log(response.data);
+//         setApiState(response.data);
+//       } catch (error) {
+//         console.log(Error);
+//       }
+//     })();
+//   });
+
+//   return (
+// 		<div>
+// 			Different ways to fetch Data
+//       {{apiData}}
+// 		</div>
+// 	);
+// }
+// export default App;
