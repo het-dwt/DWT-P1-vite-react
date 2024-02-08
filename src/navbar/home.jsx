cb19e56530044e30f77b72e4001ec1ce5cdb9649
het-dwt <156792047+het-dwt@users.noreply.github.com>
2024-02-08 19:08:27 +0530

    Add clock MUI

--- src/navbar/home.jsx
+++ src/navbar/home.jsx
@@ -1,13 +1,16 @@
 import "./home.css";
 import React from "react";
 import Clock from "./clock";
+import Paper from "@mui/material/Paper";
 
 const Home = () => {
   return (
     <div className="home">
       <div id="bg-home">
         <div id="clockParent">
-          <Clock />
+          <Paper elevation={4}>
+            <Clock />
+          </Paper>
         </div>
       </div>
     </div>
